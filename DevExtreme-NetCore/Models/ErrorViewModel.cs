31250634f6a5cfbb507d2f358e3e04db25689287
oruchankural <oruchankural@gmail.com>
2022-10-08 15:44:01 +0300

    Add project files.

---
+++ DevExtreme-NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevExtreme_NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
